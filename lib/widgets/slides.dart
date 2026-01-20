14620776e33902432e7ee313c09fbd65abab31b2
NikhilD1998 <nikhildadab@hotmail.com>
2026-01-20 13:09:42 +0530

    refactor: convert Slides widget to StatefulWidget and implement CarouselSlider for improved card display

--- lib/widgets/slides.dart
+++ lib/widgets/slides.dart
@@ -1,21 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:mypcot_assignment/widgets/orders_card_one.dart';
-import 'package:mypcot_assignment/widgets/orders_card_three.dart';
 import 'package:mypcot_assignment/widgets/orders_card_two.dart';
+import 'package:mypcot_assignment/widgets/orders_card_three.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class Slides extends StatelessWidget {
+class Slides extends StatefulWidget {
   const Slides({super.key});
 
   @override
+  State<Slides> createState() => _SlidesState();
+}
+
+class _SlidesState extends State<Slides> {
+  final List<Widget> _cards = const [
+    OrdersCardOne(),
+    OrdersCardTwo(),
+    OrdersCardThree(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        OrdersCardOne(),
-        SizedBox(height: 20),
-        OrdersCardTwo(),
-        SizedBox(height: 20),
-        OrdersCardThree(),
-      ],
+    return CarouselSlider(
+      items: _cards
+          .map(
+            (card) => Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0,
+              ), // Add horizontal space between cards
+              child: card,
+            ),
+          )
+          .toList(),
+      options: CarouselOptions(
+        height: 250,
+        autoPlay: true,
+        enlargeCenterPage: false,
+        viewportFraction: 1,
+        enableInfiniteScroll: true,
+        autoPlayInterval: const Duration(seconds: 3),
+        autoPlayAnimationDuration: const Duration(milliseconds: 800),
+        scrollDirection: Axis.horizontal,
+        padEnds: false,
+        clipBehavior: Clip.none,
+      ),
     );
   }
 }
